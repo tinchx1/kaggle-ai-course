9923b41bb1237c9b7729b31b9d494be881014558
tinchx1 <tinchomarchi34@gmail.com>
2025-03-16 19:38:46 -0300

    add pandas indexing examples for loc and iloc methods

---
+++ pandas/indexing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = pd.DataFrame({
+    'A': [10, 20, 30],
+    'B': [40, 50, 60]
+}, index=['fila1', 'fila2', 'fila3'])
+
+#loc (Label-based indexing - Indexación por etiquetas
+print(data.loc['fila2'])  # Accede a la fila con índice 'fila2'
+print(data.loc[['fila1', 'fila3'], 'B'])  # Accede a valores específicos
+
+
+#iloc (Integer-based indexing - Indexación basada en enteros
+print(data.iloc[1])  # Accede a la segunda fila (índice 1)
+print(data.iloc[:, 0])  # Accede a la primera columna (índice 0)
+print(data.iloc[0:2, 1])  # Accede a filas 0 y 1 de la columna 1
+
+#In this case `df.iloc[0:1000]` will return 1000 entries, while `df.loc[0:1000]` return 1001 of them! To get 1000 elements using `loc`, you will need to go one lower and ask for `df.iloc[0:999]`. 

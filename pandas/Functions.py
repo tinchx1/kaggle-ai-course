d84bf6a196f47237a7ecfe3951a3d6c49d5bfc68
tinchx1 <tinchomarchi34@gmail.com>
2025-03-18 19:14:32 -0300

    uncomment descriptor counting logic in Functions.py

--- pandas/Functions.py
+++ pandas/Functions.py
@@ -1,5 +1,5 @@
-#n_trop = reviews.description.map(lambda desc: "tropical" in desc).sum()
-#n_fruity = reviews.description.map(lambda desc: "fruity" in desc).sum()
-#descriptor_counts = pd.Series([n_trop, n_fruity], index=['tropical', 'fruity'])
+n_trop = reviews.description.map(lambda desc: "tropical" in desc).sum()
+n_fruity = reviews.description.map(lambda desc: "fruity" in desc).sum()
+descriptor_counts = pd.Series([n_trop, n_fruity], index=['tropical', 'fruity'])
 
 
